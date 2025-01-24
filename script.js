860643dac14f71550756b396ca15ebae35f8b487
danielkuciej <biuro.symmetry@gmail.com>
2025-01-24 13:07:56 +0100

    Finished queries (created mobile navigation)

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 "use script";
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
